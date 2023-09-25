cd1681ffd9aab21628ae6cf9d5009188649e7f73
sam <sublimecraftftw@gmail.com>
2023-09-25 02:34:31 -0700

    Animate tables (needs testing)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,106 +1,149 @@
-import * as React from "react"
+import * as React from "react";
+import { useAnimatedForwardRef } from "~/hooks/useAnimatedForwardRef";
 
-import { cn } from "~/styles/utils"
+import { cn } from "~/styles/utils";
 
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    />
-  </div>
-))
-Table.displayName = "Table"
+>(({ className, ...props }, ref) => {
+  const [tableRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <div className="relative w-full overflow-auto">
+      <table
+        ref={tableRef}
+        className={cn("w-full caption-bottom text-sm", className)}
+        {...props}
+      />
+    </div>
+  );
+});
+Table.displayName = "Table";
 
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
-))
-TableHeader.displayName = "TableHeader"
+>(({ className, ...props }, ref) => {
+  const [headerRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <thead
+      ref={headerRef}
+      className={cn("[&_tr]:border-b", className)}
+      {...props}
+    />
+  );
+});
+TableHeader.displayName = "TableHeader";
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <tbody
-    ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
-    {...props}
-  />
-))
-TableBody.displayName = "TableBody"
+>(({ className, ...props }, ref) => {
+  const [bodyRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <tbody
+      ref={bodyRef}
+      className={cn("[&_tr:last-child]:border-0", className)}
+      {...props}
+    />
+  );
+});
+TableBody.displayName = "TableBody";
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <tfoot
-    ref={ref}
-    className={cn("bg-primary font-medium text-primary-foreground", className)}
-    {...props}
-  />
-))
-TableFooter.displayName = "TableFooter"
+>(({ className, ...props }, ref) => {
+  const [footerRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <tfoot
+      ref={footerRef}
+      className={cn(
+        "bg-primary font-medium text-primary-foreground",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
+TableFooter.displayName = "TableFooter";
 
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
-  <tr
-    ref={ref}
-    className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
-      className
-    )}
-    {...props}
-  />
-))
-TableRow.displayName = "TableRow"
+>(({ className, ...props }, ref) => {
+  const [rowRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <tr
+      ref={rowRef}
+      className={cn(
+        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
+TableRow.displayName = "TableRow";
 
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
-  <th
-    ref={ref}
-    className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
-      className
-    )}
-    {...props}
-  />
-))
-TableHead.displayName = "TableHead"
+>(({ className, ...props }, ref) => {
+  const [headRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <th
+      ref={headRef}
+      className={cn(
+        "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
+TableHead.displayName = "TableHead";
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
-  <td
-    ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
-    {...props}
-  />
-))
-TableCell.displayName = "TableCell"
+>(({ className, ...props }, ref) => {
+  const [cellRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <td
+      ref={cellRef}
+      className={cn(
+        "p-4 align-middle [&:has([role=checkbox])]:pr-0",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
+TableCell.displayName = "TableCell";
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
->(({ className, ...props }, ref) => (
-  <caption
-    ref={ref}
-    className={cn("mt-4 text-sm text-muted-foreground", className)}
-    {...props}
-  />
-))
-TableCaption.displayName = "TableCaption"
+>(({ className, ...props }, ref) => {
+  const [captionRef] = useAnimatedForwardRef(ref);
+
+  return (
+    <caption
+      ref={captionRef}
+      className={cn("mt-4 text-sm text-muted-foreground", className)}
+      {...props}
+    />
+  );
+});
+TableCaption.displayName = "TableCaption";
 
 export {
   Table,
@@ -111,4 +154,4 @@ export {
   TableRow,
   TableCell,
   TableCaption,
-}
+};
