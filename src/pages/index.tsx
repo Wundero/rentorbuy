ac94c9fe598bb45668cf4493aaf2620c45d8f3d4
sam <sublimecraftftw@gmail.com>
2023-09-25 02:33:52 -0700

    Initial look

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+import { CurrentSettings } from "~/components/CurrentSettings";
+import { ThemeButton } from "~/components/ThemeButton";
 
 export default function Home() {
-
   return (
     <>
-      <main className="bg-foreground h-full text-background flex items-center justify-center">
-        hi
+      <main className="relative flex h-full items-center justify-center bg-background text-foreground">
+        <div className="absolute right-0 top-0 z-10 p-5 flex gap-2">
+          <CurrentSettings />
+          <ThemeButton />
+        </div>
+        <div className="flex justify-around w-full">
+          <section className="">TODO table</section>
+          <section className="">TODO chart</section>
+        </div>
       </main>
     </>
   );
